80b489b9ef2bf7bccb8b971a134028b5ee64b15d
dave-the-scientist <curran.dave.m@gmail.com>
2015-07-30 16:40:42 -0600

    v1.1

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 from distutils.core import setup
 
-setup(name='FiGMoP', version='1.0',
+setup(name='FiGMoP', version='1.1',
       description='FInding Genes using MOtif Patterns',
       author='David Curran',
       author_email='curran.dave.m@gmail.com',
-      url='https://github.com/dave-the-scientist',
+      url='https://github.com/dave-the-scientist/figmop',
       long_description=open('README').read(),
       license='LICENSE',
       scripts=['figmop'],
